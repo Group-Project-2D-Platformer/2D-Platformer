1ee53f4fd53b39070dbc11983c26c17b4af55d54
Fraser-Napier <fraser.orr1@gmail.com>
2021-05-06 10:42:19 +0100

    added lvl2 background

--- Group Project/Assets/Scripts/KillPlayer.cs
+++ Group Project/Assets/Scripts/KillPlayer.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class KillPlayer : MonoBehaviour
 {
     [SerializeField]Transform spawnPoint;
-
+    public GameObject background;
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.transform.CompareTag("Player"))
         {
             col.transform.position = spawnPoint.position;
+            background.active = false;
         }
     }
 
